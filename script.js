dab0af7ee1be4ec6d6b689f8daa860191afbe264
Radhwan-Abdulkareem1 <deine_github_email@example.com>
2026-03-15 17:22:21 +0100

    Alle datein angepasst. inbesonder js und html bzg. dialog: figure und figcaption henzugefügt auf Aufforderung von Dozenten.

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
 let myImgs = [
 
-    {
-        pictures: [
-
-            { adress: "ahmadardity-quran-4951037_1280.jpg", name: "Quran" },
-            { adress: "ben_kerckx-mosque-6835469_1280.jpg", name: "Mosque" },
-            { adress: "freebiespic-quran-4178711_1280.jpg", name: "Quran2" },
-            { adress: "glady-mecca-66966_1280.jpg", name: "Mekkah from Mountain" },
-            { adress: "hnsarowar385-mosque-8328077_1280.jpg", name: "Aqsa Mosque" },
-            { adress: "konevi-cami-4190404_1280.jpg", name: "Madina" },
-            { adress: "konevi-kaaba-4372252_1280.jpg", name: "Mahram Mosque" },
-            { adress: "mdjihadhossen-islam-8519137_1280.jpg", name: "Quran3" },
-            { adress: "rwayne307-al-aqsa-mosque-196846_1280.jpg", name: "Aqsa Mosque" },
-            { adress: "shamsherniazi-birds-5407779_1280.jpg", name: "Mosque" },
-        ]
-    }
+    { adress: "ahmadardity-quran-4951037_1280.jpg", name: "Quran", description: "Islamische Bücher" },
+    { adress: "ben_kerckx-mosque-6835469_1280.jpg", name: "Mosque", description: "Ein Moschee" },
+    { adress: "freebiespic-quran-4178711_1280.jpg", name: "Quran2", description: "Ein Quran mit spanischer Übersetzung" },
+    { adress: "glady-mecca-66966_1280.jpg", name: "Mekkah from Mountain", description: "Ein Bild von Mekkah" },
+    { adress: "hnsarowar385-mosque-8328077_1280.jpg", name: "Aqsa Mosque", description: "Ein Bild bon Aqsa Moschee im Quds" },
+    { adress: "konevi-cami-4190404_1280.jpg", name: "Madina", description: "Ein Bild von Medina" },
+    { adress: "konevi-kaaba-4372252_1280.jpg", name: "Mahram Mosque", description: "Mahram Moschee in Mekkah" },
+    { adress: "mdjihadhossen-islam-8519137_1280.jpg", name: "Quran3", description: "Ein Bild von Quran" },
+    { adress: "rwayne307-al-aqsa-mosque-196846_1280.jpg", name: "Aqsa Mosque", description: "Aqsa Moschee im Quds" },
+    { adress: "shamsherniazi-birds-5407779_1280.jpg", name: "Mosque", description: "Ein Moschee in einem islamischem Stadt" }
 ]; // das ist unser Array mit den unterschiedlichen Objekten! 
 
 let imgOutput = document.getElementById("imgs"); // hiermit haben wir zugriff auf unser div mit dem id imgs
 
-for (let i = 0; i < myImgs[0].pictures.length; i++) {
+for (let i = 0; i < myImgs.length; i++) {
 
-    let picture = myImgs[0].pictures[i]; //damit haben wir Zugriff auf objekts in unserem array.
+    let picture = myImgs[i]; //damit haben wir Zugriff auf objekts in unserem array.
 
-    imgOutput.innerHTML += `<button class="img-button" onclick="openDialog(${i})">
-    <img src="./img/imgs/${picture.adress}" class="main-imgs"></button>`;
-    //mit der Forschleife tun wir: 1. div in einem button umgewandelt. 
+    imgOutput.innerHTML += `<img src="./img/imgs/${picture.adress}" class="main-imgs img-button" onclick="openDialog(${i})">`;
+    //mit der Forschleife tun wir: 1. div in einem button umgewandelt.
     // 2. im button haben wir Bild mit der jeweiligen Adressen...
 }
 
 let dialogRef = document.getElementById("dialog"); //Dialog rufen.
 let dialogImage = document.getElementById("dialogImage"); //img im dialog rufen.
 let dialogTitle = document.getElementById("pictureheadline"); // Überschrift im Dialog rufen
+let imageDescription = document.getElementById("imageCaption"); // Beschreibung des Bildes im Dialog.
 let currentIndex = 0; // das ist der Startposition.
 
 function openDialog(index) {
 
     currentIndex = index; // currentindex =  das Bild was geklickt wurde.
-    let selectedImage = myImgs[0].pictures[currentIndex]; // selectedImage =  das Bild was geklickt wurde.
+    let selectedImage = myImgs[currentIndex]; // selectedImage =  das Bild was geklickt wurde.
     dialogImage.src = `./img/imgs/${selectedImage.adress}`; //Das Bild im Dialog anzeigen
     dialogTitle.textContent = selectedImage.name; // H1 im Dialog umschreiben
+    imageDescription.textContent = selectedImage.description;
     dialogRef.showModal(); // Dialog öffnen
     showPositionOfImages();
 }
@@ -50,7 +46,7 @@ function closeDialog() {
 
 function nextImage() {
     currentIndex++; // nächste Position von currentIndex aufrufen
-    if (currentIndex >= myImgs[0].pictures.length) {
+    if (currentIndex >= myImgs.length) {
         currentIndex = 0; //Startet vom Beginn wieder!
     }
     updateDialogImage();
@@ -59,7 +55,7 @@ function nextImage() {
 function previousImage() {
     currentIndex--; // letzte Position von currentIndex aufrufen
     if (currentIndex < 0) {
-        currentIndex = myImgs[0].pictures.length - 1; // Zum letzten Bild in Array springen.
+        currentIndex = myImgs.length - 1; // Zum letzten Bild in Array springen.
     }
 
     updateDialogImage();
@@ -68,10 +64,11 @@ function previousImage() {
 
 // UpdateDialogImage ist dafür da, wenn nextImagegedrückt wird, dass Dialog neugeladen wird.
 function updateDialogImage() {
-    let selectedImage = myImgs[0].pictures[currentIndex]; //selectedImage =  das Bild was geklickt wurde.
+    let selectedImage = myImgs[currentIndex]; //selectedImage =  das Bild was geklickt wurde.
 
     dialogImage.src = `./img/imgs/${selectedImage.adress}`; // Bild wird neu ausgewählt.
     dialogTitle.textContent = selectedImage.name; // Bildüberschrift wird neu ausgewählt.
+    imageDescription.textContent = selectedImage.description; // Bilderbeschreibung wird neu ausgewählt.
     showPositionOfImages();
 }
 
@@ -86,6 +83,6 @@ dialogRef.addEventListener("click", (event) => {
 let spanRef = document.getElementById("imagePosition");
 
 function showPositionOfImages() {
-    let total = myImgs[0].pictures.length;
+    let total = myImgs.length;
     spanRef.textContent = `${currentIndex + 1} / ${total}`;
 }
