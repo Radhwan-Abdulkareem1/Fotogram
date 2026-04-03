8551437ded5d968b66b48f479b65b3ac29782824
Radhwan-Abdulkareem1 <deine_github_email@example.com>
2026-04-03 23:47:52 +0200

    1. Main Padding Problem für große Bildschirme behoben.
    2. img buttons @media Größen angepasst.

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let myImgs = [
     { adress: "ahmadardity-quran-4951037_1280.jpg", name: "Quran", description: "Islamische Bücher" },
     { adress: "ben_kerckx-mosque-6835469_1280.jpg", name: "Moschee", description: "Ein Moschee" },
     { adress: "freebiespic-quran-4178711_1280.jpg", name: "Quran", description: "Ein Quran mit spanischer Übersetzung" },
-    { adress: "glady-mecca-66966_1280.jpg", name: "Mekkah vom Berg aus", description: "Ein Bild von Mekkah" },
-    { adress: "hnsarowar385-mosque-8328077_1280.jpg", name: "Aqsa Moschee", description: "Ein Bild bon Aqsa Moschee im Quds" },
-    { adress: "konevi-cami-4190404_1280.jpg", name: "Madina", description: "Ein Bild von Medina" },
+    { adress: "glady-mecca-66966_1280.jpg", name: "Mekkah vom Berg aus", description: "Mekkah" },
+    { adress: "hnsarowar385-mosque-8328077_1280.jpg", name: "Aqsa Moschee", description: "Aqsa Moschee im Quds" },
+    { adress: "konevi-cami-4190404_1280.jpg", name: "Madina", description: "Medina" },
     { adress: "konevi-kaaba-4372252_1280.jpg", name: "Mahram Moschee", description: "Mahram Moschee in Mekkah" },
-    { adress: "mdjihadhossen-islam-8519137_1280.jpg", name: "Quran", description: "Ein Bild von Quran" },
+    { adress: "mdjihadhossen-islam-8519137_1280.jpg", name: "Quran", description: "Quran" },
     { adress: "rwayne307-al-aqsa-mosque-196846_1280.jpg", name: "Aqsa Moschee", description: "Aqsa Moschee im Quds" },
     { adress: "shamsherniazi-birds-5407779_1280.jpg", name: "Moschee", description: "Ein Moschee in einem islamischem Stadt" }
 ]; // das ist unser Array mit den unterschiedlichen Objekten! 
@@ -18,7 +18,7 @@ for (let i = 0; i < myImgs.length; i++) {
 
     let picture = myImgs[i]; //damit haben wir Zugriff auf objekts in unserem array.
 
-    imgOutput.innerHTML += `<button data-index="${i}" class="imgs"><img src="./img/imgs/${picture.adress}" class="imgs" alt="${picture.name}"></button>`;
+    imgOutput.innerHTML += `<button data-index="${i}" class="img-button"><img src="./img/imgs/${picture.adress}" class="imgs" alt="${picture.name}"></button>`;
     //mit der Forschleife tun wir: 1. div in einem button umgewandelt.
     // 2. im button haben wir Bild mit der jeweiligen Adressen...
 }
@@ -26,9 +26,9 @@ for (let i = 0; i < myImgs.length; i++) {
 addClickEvents();
 
 function addClickEvents() {
-    document.querySelectorAll(".imgs").forEach(btn => {
+    document.querySelectorAll(".img-button").forEach(btn => {
         btn.addEventListener('click', (e) => {
-            let index = e.currentTarget.dataset.index;
+            let index = Number(e.currentTarget.dataset.index);
             openDialog(index);
         })
     })
